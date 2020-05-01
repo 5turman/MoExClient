cd46d131102adfbc38ef7f15652d3ba84fe1072c
Andrey Babinov <andrey.babinov@gmail.com>
2020-05-01 13:10:29 +0300

    Update gradle, kotlin & dependencies

--- app/src/main/kotlin/org/example/moex/ui/shares/SharesViewModel.kt
+++ app/src/main/kotlin/org/example/moex/ui/shares/SharesViewModel.kt
@@ -5,8 +5,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asFlow
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.combine
